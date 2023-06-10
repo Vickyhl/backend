0197235c07c1fe799cfeb304e628a93ba33fd85f
Vicky <vickyhl@ac.sce.ac.il>
2023-06-10 16:08:14 +0300

    server code update

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://eaf-2.vercel.app");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
